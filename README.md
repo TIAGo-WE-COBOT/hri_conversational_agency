ac82c005ed7769df5419e0f5bc1feefc031ca2f9
Luca <luca6.pozzi@mail.polimi.it>
2025-04-07 19:07:56 +0200

    remove support for OpenAI API

--- README.md
+++ README.md
@@ -4,28 +4,31 @@ This package is a container for ROS modules, developed for easy integration of c
 
 ## Installation
 
-TODO.
+* Clone the repo with
+    ```
+    git clone ...
+    ```
+* Install the common requirements with
+    ```
+    pip install -r requirements.txt
+    ```
+> [!NOTE]
+> The above command will not install the requirements for any specific backend (as OpenAI-GPT or Meta LLama). As such, you will only be able to run a pre-scripted bot as in []
 
 ## Setup
 
-### OpenAI
+<!--### OpenAI
 
-- Get your API key from [this link](https://platform.openai.com/account/api-keys)
-- Set it in `src/conversational_agency/openai_utils/cfg.py`
+- Get your API key from [this link](https://platform.openai.com/account/api-keys) and copy it to your clipboard.
+- Create a file named `.env` in the package root, if you do not already have one.
+- Open the `.env` file and add the following line
+    ```
+    OPENAI_API_KEY=<your_api_key>
+    ```
+    where `<your_api_key>` has to be susbstituted with the actual API key obtained at the first step.
 
-:warning: This will set your personal API key for your local version of the code. If you push it on Github, your private API will be visible to any user having access to your repo. To prevent this, follow the instructions below.
-
-- In a terminal, move to this repo and run
-```
-git update-index --skip-worktree src/conversational_agency/openai_utils/cfg.py
-```
-
-to prevent the file with your API key to be uploaded in Github on push. 
-
-- If, for any reason, you are willing to save in Git/Github changes made to the `cfg.py` file revert the previous command with
-```
-git update-index --skip-worktree src/conversational_agency/openai_utils/cfg.py
-```
+> [!IMPORTANT]
+> Make sure that the `.gitignore` file includes a `.env` or `*.env` entry to avoid the file with the API key being tracked by Git. -->
 
 ## How to run
 
@@ -35,14 +38,9 @@ TODO.
 
 TODO.
 
+<!--
 ## Troubleshooting
 
 ### - openai.error.AuthenticationError: \<empty message\>
 The error is tracked in this [Github issue](https://github.com/openai/openai-python/issues/464). Try to generate a new API key and replace the existing one.
-
-## TODO
-
-- [ ] modify the package name to `hri_conversational_agency` in the README
-- [ ] set the API key in a different way. As of now, one cannot commit changes to (e.g.) `PROMPT_TEMPLATE`
-    - [ ] env variable
-    - [ ] different file
+-->
--- scripts/chat.py
+++ scripts/chat.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
-''' This script is a template to develop your own node implementing a conversational agent. One should override listen and talk methods to define custom behavior.
+''' This node simply waits for a request from the user and then generates a response using the selected conversational agent.
+The idea of this node is to implement a standard interface for several conversational agents. The core implementation of the agent should be located in the `hri_conversational_agency/src` subfolder.
+Any higher-level interaction coordination should be implemented in a state-machine like node, sending requests to this node and receiving responses.
 '''
-from conversational_agency.openai_utils.chatter import OpenAIChatter
+import argparse
 
 import rospy
 from std_msgs.msg import String
 
-class ChatBot():
-    def __init__(self):
-        # Initialize the OpenAI model to generate responses
-        self.ai_chatter = OpenAIChatter()
+class ChatBotNode():
+    def __init__(self, backend='dummy', **kwargs):
+        # Initialize the agent to generate responses
+        if backend == 'dummy':
+            from hri_conversational_agency.dummy import DummyChatter
+            self.chatter = DummyChatter()
+        #elif backend == 'another':
+        #    pass
+        else:
+            raise NotImplementedError("The backend '{}' is not implemented. See `src` folder for available backends, or implement your own bindings for a new backend.".format(backend))
         # Listen to user input
         self.req_sub = rospy.Subscriber('request',
                                         String, 
-                                        self.listen
+                                        self.respond
                                         )
         # Initialize the publisher to broadcast the model response
         self.res_pub = rospy.Publisher('response',
@@ -22,21 +30,28 @@ class ChatBot():
                                        queue_size=1
                                        )
         self.text = ""
-        self.is_speech_recognized = False
-    
-    def listen(self, msg):
-        self.talk(msg.data)
 
-    def talk(self, in_text):
-        prompt = self.ai_chatter.generate_prompt(in_text)
-        print(prompt)
-        ans = self.ai_chatter.generate_response(prompt)
-        self.tts_pub.publish(ans)
+    def respond(self, msg):
+        ans = self.chatter.generate_response(msg.data)
+        self.res_pub.publish(ans)
         self.text = ""
     
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Implement a '
+    )
+    parser.add_argument('-b', '--backend', type=str, 
+                        default='dummy',
+                        description="The backend used to implement the conversational agent to use. Available backends: dummy, openai"
+                        )
+    parser.parse_args()
     rospy.init_node('chatbot')
-    cb = ChatBot()
+    cb = ChatBotNode(backend=parser.args.backend, 
+                     **dict((k,v) 
+                            for k,v in vars(parser.args).items() 
+                            if k!='backend'
+                            )
+                    )
+    rospy.on_shutdown(cb.chatter.log.logfile_close)
     try:
         rospy.spin()
     except KeyboardInterrupt:
